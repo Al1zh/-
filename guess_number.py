1c088c77e7edda2921972cdc43c6a87f46b6ea64
Алижан <manasbayev00@gmail.com>
2026-04-20 21:48:13 +0600

    Заменил близко на горячо и починил иф с пасхалкой

--- guess_number.py
+++ guess_number.py
@@ -12,18 +12,18 @@ def igra():
                 break
             prinimatel=int(gues)
             popitki+=1
+            if prinimatel==67:
+                print("Хахахаха! Это же сиксеееееевен!")
             raznica=abs(rannum-prinimatel)
             if prinimatel==rannum:
                 print(f"ВЫ ОТГАДАЛИ ЙОУ!!!! Вы угадали за {popitki} попыток. Я бы лучше сделал.")
                 break
-            elif prinimatel==67:
-                print("Хахахаха! Это же сиксеееееевен!")
             elif raznica>100:
                 print("Очень холодно")
             elif raznica>10:
                 print("Холодно")
             else:
-                print("близко")
+                print("Горячо")
         except ValueError:
             print("Ты угараешь?")
 igra()
